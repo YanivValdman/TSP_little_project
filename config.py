61646e9575380a3ef8a14ecfefdbdf2efc08d802
Yaniv or Rashel who knows <yanivvaldman@campus.technion.ac.il>
2025-07-16 12:46:55 +0300

    removing unnecessary files

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration module for tracker settings
+
+# The ArUco marker ID to track as the moving point
+MOVING_ID = 10
+
+# The expected number of TSP points (targets to visit)
+EXPECTED_TSP_POINTS = 5  # Change this to desired setup
